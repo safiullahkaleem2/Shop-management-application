42947b0f84781a356473082a4a33be1fb71ccdd9
safiullahkaleem2 <safiullahkaleem867@gmail.com>
2023-03-08 21:57:56 -0800

    Phase 2 of the Project

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,7 @@
 package ui;
 
 import java.io.FileNotFoundException;
-// this class runs Shopmanagement application
+// this class runs Shop management application
 
 public class Main {
     public static void main(String[] args) {
